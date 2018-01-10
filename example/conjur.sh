bc05bee7923c7ed7c050071eacabf90f6aef7d81
Kevin Gilpin <kgilpin@gmail.com>
2018-01-10 09:18:43 -0500

    remove 'example'

--- example/conjur.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash -ex
-
-conjur policy load root example/conjur.yml
-conjur variable values add pg/username conjur
-conjur variable values add pg/password conjur
-conjur variable values add pg/url pg:5432

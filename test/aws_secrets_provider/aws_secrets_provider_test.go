f3a6f350aab791fda87ecec57261fd3fd0091bea
Kumbirai Tanekha <kumbirai.tanekha@gmail.com>
2018-04-04 22:32:54 +0100

    Test AWS Secrets provider can be created and has expected name

---
+++ test/aws_secrets_provider/aws_secrets_provider_test.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/cyberark/secretless-broker/internal/app/secretless/providers"
+	plugin_v1 "github.com/cyberark/secretless-broker/pkg/secretless/plugin/v1"
+	_ "github.com/joho/godotenv/autoload"
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestAWSSecrets_Provider(t *testing.T) {
+	var err error
+	var provider plugin_v1.Provider
+
+	name := "aws"
+
+	options := plugin_v1.ProviderOptions{
+		Name: name,
+	}
+
+	Convey("Can create the AWS Secrets provider", t, func() {
+		provider, err = providers.ProviderFactories[name](options)
+		So(err, ShouldBeNil)
+	})
+
+	Convey("Has the expected provider name", t, func() {
+		So(provider.GetName(), ShouldEqual, "aws")
+	})
+}

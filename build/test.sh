5c53062f705f53d3acb6ae365ec74fcef30e6ac0
Geri Jennings <geri.jennings@cyberark.com>
2018-05-11 15:39:57 -0400

    add Jenkinsfile

--- build/test.sh
+++ build/test.sh
@@ -1,5 +1,10 @@
 #!/bin/bash -ex
 
+project_dir=$PWD
+touch $project_dir/test/junit.output
+
+go get -u github.com/jstemmer/go-junit-report
+
 cd test
 
 exit_status="0"
@@ -9,7 +14,7 @@ for dir in */; do
     [[ ! -f ./stop.sh ]] || ./stop.sh || true
     ./start.sh
     set +e
-    ./test.sh
+    ./test.sh | tee -a $project_dir/test/junit.output
     last_status="$?"
     if [[ "$exit_status" -eq "0" && "$last_status" -ne "0" ]]; then
       exit_status="$last_status"
@@ -19,5 +24,7 @@ for dir in */; do
   fi
   cd ..
 done
+
+cat $project_dir/test/junit.output | go-junit-report > $project_dir/test/junit.xml
 
 exit $exit_status

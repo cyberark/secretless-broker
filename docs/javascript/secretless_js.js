27c7744bfc9e6a0f8c7be7dee426577c9a192000
sigal <ssax18@gmail.com>
2018-07-03 14:36:20 +0300

    clean up/updates readme and condenses js

--- docs/javascript/secretless_js.js
+++ docs/javascript/secretless_js.js
@@ -1,13 +1,14 @@
 const nav=document.querySelector('#side-navigation');
 const topOfNav = nav.offsetTop;
+const docConClass = document.querySelector('.documentation-content').classList;
 
 function fixNav(){
 	if(window.scrollY >= topOfNav){
 		document.querySelector('.side-nav').classList.add('fixed-nav');
-		document.querySelector('.documentation-content').classList.add('fixed-nav-documentation');
+		docConClass.add('fixed-nav-documentation');
 	} else{
 		document.querySelector('.side-nav').classList.remove('fixed-nav');
-		document.querySelector('.documentation-content').classList.remove('fixed-nav-documentation');
+		docConClass.remove('fixed-nav-documentation');
 
 	}
 }

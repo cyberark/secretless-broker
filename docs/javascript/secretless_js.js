4a819f5c4610f6f1badd5d790b47e24d21291427
Dustin Byrne <dbyrne@conjur.net>
2018-07-12 15:41:54 -0400

    Add tabs to landing page quickstart
    You may now select PostgreSQL/HTTP/SSH quickstarts by clicking the
    appropriate button.

--- docs/javascript/secretless_js.js
+++ docs/javascript/secretless_js.js
@@ -13,3 +13,39 @@ function fixNav(){
 	}
 }
 window.addEventListener('scroll', fixNav);
+
+
+// showTab
+// Traverses up the DOM and finds `tab-content` class elements which share a
+// common root with the `event` source. Once found, all `tab-content` elements
+// are hidden, except for the element whose `id` matches `tabName`.
+function showTab(event, tabName) {
+	// Toggle `active` class on sibling buttons
+	var srcElement = event.srcElement;
+	const parentElement = srcElement.parentElement;
+	const srcSiblings = parentElement.getElementsByClassName('button');
+	for (var i = 0; i < srcSiblings.length; ++i) {
+		srcSiblings[i].classList.remove('active');
+	}
+	srcElement.classList.add('active');
+
+	// Find a common root between the event source and `tab-content` elements
+	var closestTabs = [];
+	while (srcElement != null) {
+		const tabs = srcElement.getElementsByClassName('tab-content');
+		if (tabs.length > 0) {
+			closestTabs = tabs;
+			break;
+		}
+
+		srcElement = srcElement.parentElement;
+	}
+
+	// Toggle `tab-content` visibility
+	for (var i = 0; i < closestTabs.length; ++i) {
+		const tab = closestTabs[i];
+		const display = tab.id === tabName ? 'block' : 'none';
+
+		tab.style.display = display;
+	}
+}

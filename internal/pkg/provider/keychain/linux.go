3f6e73fdd5578333fbb3caccbada2308303eadb0
Srdjan Grubor <sgnn7@sgnn7.org>
2018-07-20 11:17:57 -0500

    Fixed non-functional check_style
    Parsing of the error line length and lack of error checking for retvals
    made the script not behave expectedly so this is fixed now. Fixed one
    minor error with styling that was breaking the build now that the style
    checker is working.

--- internal/pkg/provider/keychain/linux.go
+++ internal/pkg/provider/keychain/linux.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 )
 
+// GetGenericPassword returns password data for service and account
 func GetGenericPassword(service, account string) ([]byte, error) {
 	return nil, fmt.Errorf("No keychain provider for Linux (yet)")
 }

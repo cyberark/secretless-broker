4ecd9d0bfb62553de680e9333df3d5c6fe63a497
Kumbirai Tanekha <kumbirai.tanekha@gmail.com>
2018-08-16 01:39:08 +0800

    fix indentation, remove build.sh

--- cmd/mutating-webhook-service/build.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-docker build -t cyberark/secretless-sidecar-injector:latest .
--- cmd/mutating-webhook-service/pkg/inject/server.go
+++ cmd/mutating-webhook-service/pkg/inject/server.go
@@ -1,183 +1,183 @@
 package inject
 
 import (
-    "fmt"
-    "net/http"
-    "io/ioutil"
-    "encoding/json"
+	"fmt"
+	"net/http"
+	"io/ioutil"
+	"encoding/json"
 
-    "github.com/golang/glog"
-    admissionregistrationv1beta1 "k8s.io/api/admissionregistration/v1beta1"
-    corev1 "k8s.io/api/core/v1"
-    metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-    "k8s.io/apimachinery/pkg/runtime"
-    "k8s.io/apimachinery/pkg/runtime/serializer"
-    "k8s.io/kubernetes/pkg/apis/core/v1"
-    "k8s.io/api/admission/v1beta1"
+	"github.com/golang/glog"
+	admissionregistrationv1beta1 "k8s.io/api/admissionregistration/v1beta1"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/serializer"
+	"k8s.io/kubernetes/pkg/apis/core/v1"
+	"k8s.io/api/admission/v1beta1"
 )
 
 var (
-    runtimeScheme = runtime.NewScheme()
-    codecs        = serializer.NewCodecFactory(runtimeScheme)
-    deserializer  = codecs.UniversalDeserializer()
+	runtimeScheme = runtime.NewScheme()
+	codecs        = serializer.NewCodecFactory(runtimeScheme)
+	deserializer  = codecs.UniversalDeserializer()
 
-    // (https://github.com/kubernetes/kubernetes/issues/57982)
-    defaulter = runtime.ObjectDefaulter(runtimeScheme)
+	// (https://github.com/kubernetes/kubernetes/issues/57982)
+	defaulter = runtime.ObjectDefaulter(runtimeScheme)
 )
 
 const (
-    admissionWebhookAnnotationInjectKey = "sidecar-injector-webhook.secretless.io/inject"
-    admissionWebhookAnnotationConfigKey = "sidecar-injector-webhook.secretless.io/config"
-    admissionWebhookAnnotationStatusKey = "sidecar-injector-webhook.secretless.io/status"
+	admissionWebhookAnnotationInjectKey = "sidecar-injector-webhook.secretless.io/inject"
+	admissionWebhookAnnotationConfigKey = "sidecar-injector-webhook.secretless.io/config"
+	admissionWebhookAnnotationStatusKey = "sidecar-injector-webhook.secretless.io/status"
 )
 
 
 func init() {
-    _ = corev1.AddToScheme(runtimeScheme)
-    _ = admissionregistrationv1beta1.AddToScheme(runtimeScheme)
-    // defaulting with webhooks:
-    // https://github.com/kubernetes/kubernetes/issues/57982
-    _ = v1.AddToScheme(runtimeScheme)
+	_ = corev1.AddToScheme(runtimeScheme)
+	_ = admissionregistrationv1beta1.AddToScheme(runtimeScheme)
+	// defaulting with webhooks:
+	// https://github.com/kubernetes/kubernetes/issues/57982
+	_ = v1.AddToScheme(runtimeScheme)
 }
 
 // applyDefaultsWorkaround applies a defaulting on Container and Volume specs to address this issue (https://github.com/kubernetes/kubernetes/issues/57982)
 func applyDefaultsWorkaround(containers []corev1.Container, volumes []corev1.Volume) {
-    defaulter.Default(&corev1.Pod{
-        Spec: corev1.PodSpec{
-            Containers: containers,
-            Volumes:    volumes,
-        },
-    })
+	defaulter.Default(&corev1.Pod{
+		Spec: corev1.PodSpec{
+			Containers: containers,
+			Volumes:    volumes,
+		},
+	})
 }
 
 var ignoredNamespaces = []string{
-    metav1.NamespaceSystem,
-    metav1.NamespacePublic,
+	metav1.NamespaceSystem,
+	metav1.NamespacePublic,
 }
 
 type WebhookServer struct {
-    Server *http.Server
+	Server *http.Server
 }
 
 // Webhook Server parameters
 type WebhookServerParameters struct {
-    Port     int    // webhook Server port
-    CertFile string // path to the x509 certificate for https
-    KeyFile  string // path to the x509 private key matching `CertFile`
+	Port     int    // webhook Server port
+	CertFile string // path to the x509 certificate for https
+	KeyFile  string // path to the x509 private key matching `CertFile`
 }
 
 // main mutation process
 func (whsvr *WebhookServer) mutate(ar *v1beta1.AdmissionReview) *v1beta1.AdmissionResponse {
-    req := ar.Request
-    var pod corev1.Pod
-    if err := json.Unmarshal(req.Object.Raw, &pod); err != nil {
-        glog.Errorf("Could not unmarshal raw object: %v", err)
-        return &v1beta1.AdmissionResponse{
-            Result: &metav1.Status{
-                Message: err.Error(),
-            },
-        }
-    }
+	req := ar.Request
+	var pod corev1.Pod
+	if err := json.Unmarshal(req.Object.Raw, &pod); err != nil {
+		glog.Errorf("Could not unmarshal raw object: %v", err)
+		return &v1beta1.AdmissionResponse{
+			Result: &metav1.Status{
+				Message: err.Error(),
+			},
+		}
+	}
 
-    glog.Infof("AdmissionReview for Kind=%v, Namespace=%v Name=%v (%v) UID=%v rfc6902PatchOperation=%v UserInfo=%v",
-        req.Kind, req.Namespace, req.Name, pod.Name, req.UID, req.Operation, req.UserInfo)
+	glog.Infof("AdmissionReview for Kind=%v, Namespace=%v Name=%v (%v) UID=%v rfc6902PatchOperation=%v UserInfo=%v",
+		req.Kind, req.Namespace, req.Name, pod.Name, req.UID, req.Operation, req.UserInfo)
 
-    // determine whether to perform mutation
-    if !mutationRequired(ignoredNamespaces, &pod.ObjectMeta) {
-        glog.Infof("Skipping mutation for %s/%s due to policy check", pod.Namespace, pod.Name)
-        return &v1beta1.AdmissionResponse{
-            Allowed: true,
-        }
-    }
+	// determine whether to perform mutation
+	if !mutationRequired(ignoredNamespaces, &pod.ObjectMeta) {
+		glog.Infof("Skipping mutation for %s/%s due to policy check", pod.Namespace, pod.Name)
+		return &v1beta1.AdmissionResponse{
+			Allowed: true,
+		}
+	}
 
-    secretlessConfigMapName, hasKey := getSecretlessConfigMapName(&pod.ObjectMeta)
-    if !hasKey {
-        errorMsg := fmt.Sprintf("Mutation failed for %s/%s due to missing annotation %s", pod.Namespace, pod.Name, admissionWebhookAnnotationConfigKey)
+	secretlessConfigMapName, hasKey := getSecretlessConfigMapName(&pod.ObjectMeta)
+	if !hasKey {
+		errorMsg := fmt.Sprintf("Mutation failed for %s/%s due to missing annotation %s", pod.Namespace, pod.Name, admissionWebhookAnnotationConfigKey)
 
-        glog.Infof(errorMsg)
-        return &v1beta1.AdmissionResponse{
-            Result: &metav1.Status{
-                Message: errorMsg,
-            },
-        }
-    }
-    sidecarConfig := generateSidecarConfig(secretlessConfigMapName)
+		glog.Infof(errorMsg)
+		return &v1beta1.AdmissionResponse{
+			Result: &metav1.Status{
+				Message: errorMsg,
+			},
+		}
+	}
+	sidecarConfig := generateSidecarConfig(secretlessConfigMapName)
 
-    // Workaround: https://github.com/kubernetes/kubernetes/issues/57982
-    applyDefaultsWorkaround(sidecarConfig.Containers, sidecarConfig.Volumes)
-    annotations := map[string]string{admissionWebhookAnnotationStatusKey: "injected"}
-    patchBytes, err := createPatch(&pod, sidecarConfig, annotations)
-    if err != nil {
-        return &v1beta1.AdmissionResponse{
-            Result: &metav1.Status{
-                Message: err.Error(),
-            },
-        }
-    }
+	// Workaround: https://github.com/kubernetes/kubernetes/issues/57982
+	applyDefaultsWorkaround(sidecarConfig.Containers, sidecarConfig.Volumes)
+	annotations := map[string]string{admissionWebhookAnnotationStatusKey: "injected"}
+	patchBytes, err := createPatch(&pod, sidecarConfig, annotations)
+	if err != nil {
+		return &v1beta1.AdmissionResponse{
+			Result: &metav1.Status{
+				Message: err.Error(),
+			},
+		}
+	}
 
-    glog.Infof("AdmissionResponse: patch=%v\n", string(patchBytes))
-    return &v1beta1.AdmissionResponse{
-        Allowed: true,
-        Patch:   patchBytes,
-        PatchType: func() *v1beta1.PatchType {
-            pt := v1beta1.PatchTypeJSONPatch
-            return &pt
-        }(),
-    }
+	glog.Infof("AdmissionResponse: patch=%v\n", string(patchBytes))
+	return &v1beta1.AdmissionResponse{
+		Allowed: true,
+		Patch:   patchBytes,
+		PatchType: func() *v1beta1.PatchType {
+			pt := v1beta1.PatchTypeJSONPatch
+			return &pt
+		}(),
+	}
 }
 
 // Serve method for webhook Server
 func (whsvr *WebhookServer) Serve(w http.ResponseWriter, r *http.Request) {
-    var body []byte
-    if r.Body != nil {
-        if data, err := ioutil.ReadAll(r.Body); err == nil {
-            body = data
-        }
-    }
+	var body []byte
+	if r.Body != nil {
+		if data, err := ioutil.ReadAll(r.Body); err == nil {
+			body = data
+		}
+	}
 
-    if len(body) == 0 {
-        glog.Error("empty body")
-        http.Error(w, "empty body", http.StatusBadRequest)
-        return
-    }
+	if len(body) == 0 {
+		glog.Error("empty body")
+		http.Error(w, "empty body", http.StatusBadRequest)
+		return
+	}
 
-    // verify the content type is accurate
-    contentType := r.Header.Get("Content-Type")
-    if contentType != "application/json" {
-        glog.Errorf("Content-Type=%s, expect application/json", contentType)
-        http.Error(w, "invalid Content-Type, expect `application/json`", http.StatusUnsupportedMediaType)
-        return
-    }
+	// verify the content type is accurate
+	contentType := r.Header.Get("Content-Type")
+	if contentType != "application/json" {
+		glog.Errorf("Content-Type=%s, expect application/json", contentType)
+		http.Error(w, "invalid Content-Type, expect `application/json`", http.StatusUnsupportedMediaType)
+		return
+	}
 
-    var admissionResponse *v1beta1.AdmissionResponse
-    ar := v1beta1.AdmissionReview{}
-    if _, _, err := deserializer.Decode(body, nil, &ar); err != nil {
-        glog.Errorf("Can't decode body: %v", err)
-        admissionResponse = &v1beta1.AdmissionResponse{
-            Result: &metav1.Status{
-                Message: err.Error(),
-            },
-        }
-    } else {
-        admissionResponse = whsvr.mutate(&ar)
-    }
+	var admissionResponse *v1beta1.AdmissionResponse
+	ar := v1beta1.AdmissionReview{}
+	if _, _, err := deserializer.Decode(body, nil, &ar); err != nil {
+		glog.Errorf("Can't decode body: %v", err)
+		admissionResponse = &v1beta1.AdmissionResponse{
+			Result: &metav1.Status{
+				Message: err.Error(),
+			},
+		}
+	} else {
+		admissionResponse = whsvr.mutate(&ar)
+	}
 
-    admissionReview := v1beta1.AdmissionReview{}
-    if admissionResponse != nil {
-        admissionReview.Response = admissionResponse
-        if ar.Request != nil {
-            admissionReview.Response.UID = ar.Request.UID
-        }
-    }
+	admissionReview := v1beta1.AdmissionReview{}
+	if admissionResponse != nil {
+		admissionReview.Response = admissionResponse
+		if ar.Request != nil {
+			admissionReview.Response.UID = ar.Request.UID
+		}
+	}
 
-    resp, err := json.Marshal(admissionReview)
-    if err != nil {
-        glog.Errorf("Can't encode response: %v", err)
-        http.Error(w, fmt.Sprintf("could not encode response: %v", err), http.StatusInternalServerError)
-    }
-    glog.Infof("Ready to write reponse ...")
-    if _, err := w.Write(resp); err != nil {
-        glog.Errorf("Can't write response: %v", err)
-        http.Error(w, fmt.Sprintf("could not write response: %v", err), http.StatusInternalServerError)
-    }
+	resp, err := json.Marshal(admissionReview)
+	if err != nil {
+		glog.Errorf("Can't encode response: %v", err)
+		http.Error(w, fmt.Sprintf("could not encode response: %v", err), http.StatusInternalServerError)
+	}
+	glog.Infof("Ready to write reponse ...")
+	if _, err := w.Write(resp); err != nil {
+		glog.Errorf("Can't write response: %v", err)
+		http.Error(w, fmt.Sprintf("could not write response: %v", err), http.StatusInternalServerError)
+	}
 }

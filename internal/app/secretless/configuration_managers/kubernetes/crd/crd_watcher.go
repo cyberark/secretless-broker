513c0dd6d3703a1dd8be90195648c61bac2a6724
Srdjan Grubor <sgnn7@sgnn7.org>
2018-08-23 15:14:33 -0500

    Added a Kubernetes CRD-based configuration provider
    This configuration provider now enables us to create a CRD, watch for
    changes on it, and reload our configuration based on the new changes. We
    also have the ability to filter by CRD IDs to make sure we only track a
    single configuration object.

---
+++ internal/app/secretless/configuration_managers/kubernetes/crd/crd_watcher.go
@@ -0,0 +1,90 @@
+package crd
+
+import (
+	"log"
+
+	meta_v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/util/wait"
+	"k8s.io/apimachinery/pkg/watch"
+	"k8s.io/client-go/tools/cache"
+
+	api_v1 "github.com/cyberark/secretless-broker/pkg/apis/secretless.io/v1"
+	secretlessClientset "github.com/cyberark/secretless-broker/pkg/k8sclient/clientset/versioned"
+)
+
+// ResourceEventHandler is the interface for handling CRD push notification
+type ResourceEventHandler interface {
+	CRDAdded(*api_v1.Configuration)
+	CRDDeleted(*api_v1.Configuration)
+	CRDUpdated(*api_v1.Configuration, *api_v1.Configuration)
+}
+
+// RegisterCRDListener registers a CRD push-notification handler to the available
+// k8s cluster
+func RegisterCRDListener(namespace string, configSpec string, resourceEventHandler ResourceEventHandler) error {
+	log.Printf("%s: Registering CRD watcher...", PluginName)
+
+	clientConfig, err := NewKubernetesConfig()
+	if err != nil {
+		return err
+	}
+
+	clientset, err := secretlessClientset.NewForConfig(clientConfig)
+	if err != nil {
+		return err
+	}
+
+	// TODO: Watch for CRD availability
+
+	// TODO: We might not want to listen in on all namespace changes
+	watchList := &cache.ListWatch{
+		ListFunc: func(listOpts meta_v1.ListOptions) (result runtime.Object, err error) {
+			return clientset.SecretlessV1().Configurations(namespace).List(listOpts)
+		},
+		WatchFunc: func(listOpts meta_v1.ListOptions) (watch.Interface, error) {
+			return clientset.SecretlessV1().Configurations(namespace).Watch(listOpts)
+		},
+	}
+
+	_, controller := cache.NewInformer(
+		watchList,
+		&api_v1.Configuration{},
+		CRDForcedRefreshInterval,
+		cache.ResourceEventHandlerFuncs{
+			AddFunc: func(obj interface{}) {
+				configObj := obj.(*api_v1.Configuration)
+				if configObj.ObjectMeta.Name != configSpec {
+					return
+				}
+
+				log.Printf("%s: Add configuration event", PluginName)
+				log.Println(configObj.ObjectMeta.Name)
+				resourceEventHandler.CRDAdded(configObj)
+			},
+			DeleteFunc: func(obj interface{}) {
+				configObj := obj.(*api_v1.Configuration)
+				if configObj.ObjectMeta.Name != configSpec {
+					return
+				}
+
+				log.Printf("%s: Delete configuration event", PluginName)
+				resourceEventHandler.CRDDeleted(configObj)
+			},
+			UpdateFunc: func(oldObj, newObj interface{}) {
+				oldConfigObj := oldObj.(*api_v1.Configuration)
+				if oldConfigObj.ObjectMeta.Name != configSpec {
+					return
+				}
+
+				log.Printf("%s: Update/refresh configuration event", PluginName)
+				newConfigObj := newObj.(*api_v1.Configuration)
+				resourceEventHandler.CRDUpdated(oldConfigObj, newConfigObj)
+			},
+		},
+	)
+
+	go controller.Run(wait.NeverStop)
+
+	return nil
+}
---
+++ internal/app/secretless/configuration_managers/kubernetes/crd/k8s_config.go
@@ -0,0 +1,42 @@
+package crd
+
+import (
+	"log"
+	"os"
+	"path/filepath"
+
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
+)
+
+func getHomeDir() string {
+	if home := os.Getenv("HOME"); home != "" {
+		return home
+	}
+	return os.Getenv("USERPROFILE")
+}
+
+// NewKubernetesConfig tries to guess the current k8s configuration for the CRD
+// plugin to use
+func NewKubernetesConfig() (config *rest.Config, err error) {
+	// Try to use file-based config first
+	var kubeConfig string
+	if home := getHomeDir(); home != "" {
+		log.Printf("%s: Using home dir config...", PluginName)
+		kubeConfig = filepath.Join(home, ".kube", "config")
+
+		if config, err = clientcmd.BuildConfigFromFlags("", kubeConfig); err != nil {
+			return
+		}
+	}
+
+	// Otherwise try using in-cluster service account
+	if config == nil {
+		log.Printf("%s: Fetching cluster config...", PluginName)
+		if config, err = rest.InClusterConfig(); err != nil {
+			return
+		}
+	}
+
+	return config, err
+}

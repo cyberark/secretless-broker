806679bfae1e1b0789be8b158ac9c997f7673228
Srdjan Grubor <sgnn7@sgnn7.org>
2018-08-27 17:26:07 -0500

    Made configuration file reading use a configuration manager plugin
    Since we now can use configuration manager plugins, we now have our
    regular config file reading moved into this abstraction. The CLI
    interactions have not been changed so it is a fully backward-supported
    feature.

--- internal/app/secretless/configurationmanagers/configuration_managers.go
+++ internal/app/secretless/configurationmanagers/configuration_managers.go
@@ -1,11 +1,13 @@
 package configurationmanagers
 
 import (
+	"github.com/cyberark/secretless-broker/internal/app/secretless/configurationmanagers/configfile"
 	"github.com/cyberark/secretless-broker/internal/app/secretless/configurationmanagers/kubernetes/crd"
 	plugin_v1 "github.com/cyberark/secretless-broker/pkg/secretless/plugin/v1"
 )
 
 // ConfigurationManagerFactories contains the list of built-in factories
 var ConfigurationManagerFactories = map[string]func(plugin_v1.ConfigurationManagerOptions) plugin_v1.ConfigurationManager{
-	crd.PluginName: crd.ConfigurationManagerFactory,
+	configfile.PluginName: configfile.ConfigurationManagerFactory,
+	crd.PluginName:        crd.ConfigurationManagerFactory,
 }

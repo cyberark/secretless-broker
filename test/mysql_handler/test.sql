fcc7226748cf614dc0d8287647f6e06993ce1b4f
sigal <ssax18@gmail.com>
2018-10-08 14:42:17 +0300

    Add initial benchmarking for MySQL This adds the first phase of benchmarking for MySQL to further expand the Secretless benchmarks

--- test/mysql_handler/test.sql
+++ test/mysql_handler/test.sql
@@ -5,4 +5,18 @@ CREATE DATABASE testdb;
 
 CREATE TABLE testdb.test ( id int );
 
-INSERT INTO testdb.test VALUES ( 1 ), ( 2 );
+USE testdb;
+DROP PROCEDURE IF EXISTS prepare_data;
+DELIMITER $$
+CREATE PROCEDURE prepare_data()
+BEGIN
+  DECLARE i INT DEFAULT 1;
+
+  WHILE i < 100000 DO
+    INSERT INTO testdb.test (id) VALUES (i);
+    SET i = i + 1;
+  END WHILE;
+END$$
+DELIMITER ;
+
+CALL prepare_data();

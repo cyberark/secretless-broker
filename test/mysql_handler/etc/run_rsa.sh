3b9c70e011da4a82a472e20d29ab5e890957adbe
Kumbirai Tanekha <kumbirai.tanekha@gmail.com>
2018-11-30 00:15:21 +0000

    test: add tests for handler supports TLS defaulting to sslmode=require

--- test/mysql_handler/etc/run_rsa.sh
+++ test/mysql_handler/etc/run_rsa.sh
@@ -1,10 +1,8 @@
 #!/bin/bash -e
 
-mysql_ssl_rsa_setup
-
-cat << EOF > /etc/mysql/mysql.conf.d/ssl.cnf
-[mysqld]
-ssl-ca=/var/lib/mysql/ca.pem
-ssl-cert=/var/lib/mysql/server-cert.pem
-ssl-key=/var/lib/mysql/server-key.pem
-EOF
+if [[ "$NO_SSL" = "true" ]]
+then
+  echo "removing SSL support"
+  rm /var/lib/mysql/*.pem
+  echo "ssl=0" >> /etc/my.cnf
+fi

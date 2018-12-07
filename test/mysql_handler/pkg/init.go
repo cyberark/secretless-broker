cb4806a3e9e1ed168605ed856642bcf06e531930
Kumbirai Tanekha <kumbirai.tanekha@gmail.com>
2018-12-07 20:24:20 +0000

    merge env into init

--- test/mysql_handler/pkg/init.go
+++ test/mysql_handler/pkg/init.go
@@ -7,6 +7,30 @@ import (
 	"path/filepath"
 )
 
+// ENV Configuration: Verbose output mode
+//
+var Verbose = func() bool {
+	debug := os.Getenv("VERBOSE")
+	for _, truthyVal := range []string{"true", "yes", "t", "y"} {
+		if truthyVal == debug {
+			return true
+		}
+	}
+	return false
+}()
+
+// ENV Configuration: Name of Secretless host to use
+//
+// Allows us to specify a different host when doing development, for
+// faster code reloading.  See the "dev" script in this folder.
+//
+var SecretlessHost = func() string {
+	if host, ok := os.LookupEnv("SECRETLESS_HOST"); ok {
+		return host
+	}
+	return "secretless"
+}()
+
 var TestDir string
 func init() {
 	secretlessRoot, ok := os.LookupEnv("PROJECT_ROOT")

f7c3e2609c696989504165360c45707c46b05f9a
Srdjan Grubor <sgnn7@sgnn7.org>
2019-05-24 10:35:10 -0500

    Fixed up and refactored "k8s-demo" demo
    This code was pretty broken and brittle so these changes ensure that
    it's more flexible, better documented, and that it works without
    hardcoded values. code was also cleaned up in the process.

--- demos/k8s-demo/utils.sh
+++ demos/k8s-demo/utils.sh
@@ -1,6 +1,10 @@
 #!/bin/bash -e
 
-step() { printf '\n\n>>--- %s\n\n' "$1"; }
+step() {
+  echo
+  echo ">>--- $1"
+  echo
+}
 
 first_pod() {
   kubectl get pods \
@@ -18,39 +22,14 @@ wait_for_app() {
     --output jsonpath='{$.items[0].status.containerStatuses.*.ready}')" =~ (true ?)+ ]]
   do
     if [[ "$waiting" != "true" ]]; then
-      printf "Waiting for %s to be ready" "$1"
+      echo "Waiting for $1 to be ready"
       waiting=true
     fi
-    printf "."
+    echo -n "."
     sleep 3
   done
 
   if [[ "$waiting" = "true" ]]; then
-    printf "Done"
+    echo "OK"
   fi
-}
-
-# Note: In future versions of k8s we'll be able to replace this function
-# with the k8s "wait" command:
-#
-# https://kubernetes.io/docs/reference/generated/kubectl/kubectl-commands#wait
-#
-delete_ns_and_cleanup() {
-  # We don't care about the output of this command, only its return return code:
-  # success means there was something to delete, and that we must wait for
-  # deletion to fully process.  Failure means there was nothing to delete and
-  # we can return early.
-  if ! kubectl delete namespace "$1" > /dev/null 2>&1; then
-    return 0
-  fi
-
-  printf "Cleaning up old namespace"
-
-  # As long as we can still "get" the namespace, the deletion isn't done.
-  while kubectl get namespace "$1" > /dev/null 2>&1; do 
-    printf "."
-    sleep 3
-  done
-
-  printf '%s\n\n' "Done"
 }

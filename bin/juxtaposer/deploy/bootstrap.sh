7b042598979fc07a6d696ca67d1568b08421d370
Srdjan Grubor <sgnn7@sgnn7.org>
2019-06-06 10:18:25 -0500

    Added ability to deploy perftool to a mock cluster
    This adds basic ability to create a deployment specifically targetting
    the mock cluster with plans to expand it to include more-flexible
    options.

---
+++ bin/juxtaposer/deploy/bootstrap.sh
@@ -0,0 +1,7 @@
+export AUTHENTICATOR_ID=madeup-id
+export APPLICATION_SERVICE_ACCOUNT=test-app-secretless
+export DAP_ACCOUNT=example
+export DAP_NAMESPACE_NAME=conjur-geri-test
+export DAP_SSL_CERT_CONFIG_MAP=geri-test
+export DOCKER_REGISTRY_PATH=docker-registry-default.openshift-39.itci.conjur.net
+export TEST_APP_NAMESPACE_NAME=geri-test

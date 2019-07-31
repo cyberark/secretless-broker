bfa5c8f76bda41ae32fdbb14765d312e43b82a48
Srdjan Grubor <sgnn7@sgnn7.org>
2019-07-31 14:19:08 -0500

    Made MySQL connector able to guess the 3306 port if it is not provided
    Since in most cases, we know that the MySQL port is 3306, it is
    superfluous to need that in the config if the user is connecting to the
    default port setting. This commit also reorganizes the code a bit for
    better testability and adds a few tests around this feature.

--- internal/app/secretless/handlers/mysql/connection_details.go
+++ internal/app/secretless/handlers/mysql/connection_details.go
@@ -12,6 +12,46 @@ type ConnectionDetails struct {
 	Options  map[string]string
 }
 
+const DefaultMySQLPort = uint(3306)
+
+// NewConnectionDetails is a constructor of ConnectionDetails structure from a
+// map of credentials.
+func NewConnectionDetails(credentials map[string][]byte) (
+	*ConnectionDetails, error) {
+
+	connDetails := &ConnectionDetails{Options: make(map[string]string)}
+
+	if host := credentials["host"]; host != nil {
+		connDetails.Host = string(credentials["host"])
+	}
+
+	connDetails.Port = DefaultMySQLPort
+	if credentials["port"] != nil {
+		port64, _ := strconv.ParseUint(string(credentials["port"]), 10, 64)
+		connDetails.Port = uint(port64)
+	}
+
+	if credentials["username"] != nil {
+		connDetails.Username = string(credentials["username"])
+	}
+
+	if credentials["password"] != nil {
+		connDetails.Password = string(credentials["password"])
+	}
+
+	delete(credentials, "host")
+	delete(credentials, "port")
+	delete(credentials, "username")
+	delete(credentials, "password")
+
+	connDetails.Options = make(map[string]string)
+	for k, v := range credentials {
+		connDetails.Options[k] = string(v)
+	}
+
+	return connDetails, nil
+}
+
 // Address returns a string representing the network location (host and port)
 // of a MySQL server.  This is the string you would would typically use to
 // connect to the database -- eg, in cmd line tools.

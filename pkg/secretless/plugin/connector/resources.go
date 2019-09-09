7f835d378a93aa42a128d47e98b4d0029683e84f
Srdjan Grubor <sgnn7@sgnn7.org>
2019-09-09 10:27:04 -0500

    Added a method to create a new connector Resources interface
    We now have a way to instantiate a connector Resources interface via a
    backing object. This should allow us to easily invoke the NewConnector
    methods on plugins.

--- pkg/secretless/plugin/connector/resources.go
+++ pkg/secretless/plugin/connector/resources.go
@@ -15,3 +15,28 @@ type Resources interface {
 	// to record logging messages from the plugin.
 	Logger() log.Logger
 }
+
+type _resources struct {
+	config []byte
+	logger log.Logger
+}
+
+func (res *_resources) Config() []byte {
+	return res.config
+}
+
+func (res *_resources) Logger() log.Logger {
+	return res.logger
+}
+
+// NewResources creates a new Resources interface from a backing object struct
+// to be used in invocation of a new Connector
+func NewResources(
+	config []byte,
+	logger log.Logger) Resources {
+
+	return &_resources{
+		config: config,
+		logger: logger,
+	}
+}
---
+++ pkg/secretless/plugin/connector/resources_test.go
@@ -0,0 +1,22 @@
+package connector
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/cyberark/secretless-broker/internal/pkg/log"
+)
+
+func TestNewResources(t *testing.T) {
+	config := []byte("configvalue")
+	logger := log.New(false)
+
+	// Ensure that the return type is the exact type we expect
+	var resources Resources
+
+	resources = NewResources(config, logger)
+
+	assert.Equal(t, config, resources.Config())
+	assert.Equal(t, logger, resources.Logger())
+}

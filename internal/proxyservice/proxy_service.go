275b650748a45b4ba552472495f9610ee9fdec66
Jonah <jonahx@gmail.com>
2019-09-20 15:22:56 -0400

    Add ProxyServices type responsible for starting all services
    This is the main, top-level Secretless logic that starts all the
    services, and which is itself intended to be called from the secretless
    entrypoint.
    
    This commit adds support for TCP services.  Unit tests and support for
    HTTP and SSH services will arrive shortly in later commits.

--- internal/proxyservice/proxy_service.go
+++ internal/proxyservice/proxy_service.go
@@ -1,43 +1,147 @@
 package proxyservice
 
 import (
+	"net"
+	"strings"
+
+	"github.com/cyberark/secretless-broker/internal"
 	"github.com/cyberark/secretless-broker/internal/plugin"
 	v1 "github.com/cyberark/secretless-broker/internal/plugin/v1"
+	tcpproxy "github.com/cyberark/secretless-broker/internal/proxyservice/tcp"
 	"github.com/cyberark/secretless-broker/pkg/secretless"
 	v2 "github.com/cyberark/secretless-broker/pkg/secretless/config/v2"
-	"github.com/cyberark/secretless-broker/pkg/secretless/log"
+	logapi "github.com/cyberark/secretless-broker/pkg/secretless/log"
+	"github.com/cyberark/secretless-broker/pkg/secretless/plugin/connector"
+	"github.com/cyberark/secretless-broker/pkg/secretless/plugin/connector/tcp"
 )
 
 // TODO: move to impl package
 type proxyServices struct {
-	config        v2.Config
-	logger        log.Logger
-	eventNotifier v1.EventNotifier
-	availPlugins  plugin.AvailablePlugins
+	config          v2.Config
+	logger          logapi.Logger
+	eventNotifier   v1.EventNotifier
+	availPlugins    plugin.AvailablePlugins
+	runningServices []secretless.Service
 }
 
-// TODO: Rename to Call or Run and return a Stopper instead of having Stop()
-// Start starts all proxy services
-func (s *proxyServices) Start() {
-	// TODO: Implement
+type connectorResources struct {
+	logger logapi.Logger
+	config []byte
+}
 
-	// For each ProxyService:
-	//
-	// 1. Rewrap the Logger with service name prefix
-	// 2. Create the ConnectorResources object
-	// etc...
+func (cr *connectorResources) Logger() logapi.Logger {
+	return cr.logger
+}
+
+func (cr *connectorResources) Config() []byte {
+	return cr.config
+}
+
+func newConnectorResources(l logapi.Logger, cfg []byte) connector.Resources {
+	return &connectorResources{
+		logger: l,
+		config: cfg,
+	}
+}
+
+// Start starts all proxy services
+func (s *proxyServices) Start() error {
+	for _, svc := range s.servicesToStart() {
+		err := svc.Start()
+		if err != nil {
+			s.logger.Errorf("could not start proxy service: %s", err)
+			continue
+		}
+		s.runningServices = append(s.runningServices, svc)
+	}
+	return nil
 }
 
 // Stop stops all proxy services
-func (s *proxyServices) Stop() {
-	// Stop all the Service
+func (s *proxyServices) Stop() error {
+	for _, svc := range s.runningServices {
+		err := svc.Stop()
+		if err != nil {
+			s.logger.Errorf("could not stop proxy service: %s", err)
+		}
+	}
+	return nil
+}
+
+func (s *proxyServices) servicesToStart() []secretless.Service {
+	var servicesToStart []secretless.Service
+
+	tcpPlugins := s.availPlugins.TCPPlugins()
+	// httpPlugins := s.availPlugins.HTTPPlugins()
+
+	for _, svc := range s.config.Services {
+		requestedPlugin := svc.Connector //TODO: this rename is a name smell
+
+		// first check the available TCP Plugins
+		tcpPlugin, found := tcpPlugins[requestedPlugin]
+		if found {
+			if tcpSvc := s.createTCPService(svc, tcpPlugin); tcpSvc != nil {
+				servicesToStart = append(servicesToStart, tcpSvc)
+			}
+			continue
+		}
+
+		// TODO: next check available HTTP Plugins
+		// httpPlugin, found := httpPlugins[requestedPlugin]
+
+		// TODO: Deal with SSH in a hardcoded way
+
+		// Default case: not found
+		s.logger.Errorf("plugin '%s' not available.", requestedPlugin)
+	}
+	return servicesToStart
+}
+
+func (s *proxyServices) createTCPService(
+	svc *v2.Service,
+	plugin tcp.Plugin,
+) secretless.Service {
+
+	//TODO: Add validation somewhere about overlapping listenOns
+	listener, err := net.Listen("tcp", strings.TrimLeft(svc.ListenOn, "tcp://"))
+	if err != nil {
+		// TODO: Should we do more than this?
+		s.logger.Errorf("could not create listener on: %s", svc.ListenOn)
+		return nil
+	}
+
+	svcLogger := s.logger.CopyWith(svc.Name, s.logger.DebugEnabled())
+	connResources := newConnectorResources(svcLogger, svc.ConnectorConfig)
+	connector_ := plugin.NewConnector(connResources)
+
+	// Temp var required so that the function closes over the current
+	// loop value.
+	credsCopy := svc.Credentials
+	credsRetriever := func() (map[string][]byte, error) {
+		return GetSecrets(credsCopy)
+	}
+
+	newSvc, err := tcpproxy.NewProxyService(
+		connector_,
+		listener,
+		svcLogger,
+		credsRetriever,
+	)
+
+	if err != nil {
+		// TODO: Should we do more than this?
+		s.logger.Errorf("could not create proxy service '%s'", svc.Name)
+		return nil
+	}
+
+	return newSvc
 }
 
 // NewProxyServices returns a new ProxyServices instance.
 func NewProxyServices(
 	cfg v2.Config,
 	availPlugins plugin.AvailablePlugins,
-	logger log.Logger,
+	logger logapi.Logger,
 	evtNotifier v1.EventNotifier,
 ) secretless.Service {
 
@@ -52,4 +156,17 @@ func NewProxyServices(
 	//   logic uses availPlugins and config to figure out what services to start
 
 	return &secretlessObj
+}
+
+// Move this up one level, pass it down for now
+func GetSecrets(secrets []*v2.Credential) (map[string][]byte, error) {
+	providerFactories := make(map[string]func(v1.ProviderOptions) (v1.Provider, error))
+
+	for providerID, providerFactory := range internal.InternalProviders {
+		providerFactories[providerID] = providerFactory
+	}
+
+	resolver := plugin.NewResolver(providerFactories, nil, nil)
+
+	return resolver.Resolve(secrets)
 }

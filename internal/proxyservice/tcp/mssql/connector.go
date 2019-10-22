e22851a9f2ec7fa0ec288e6691363c07b202dbae
Kumbirai Tanekha <kumbirai.tanekha@gmail.com>
2019-10-22 12:37:03 +0100

    rudimentary mssql connector

--- internal/proxyservice/tcp/mssql/connector.go
+++ internal/proxyservice/tcp/mssql/connector.go
@@ -1,7 +1,11 @@
 package mssql
 
 import (
+	"context"
+	"fmt"
 	"net"
+
+	mssql "github.com/denisenkom/go-mssqldb"
 
 	"github.com/cyberark/secretless-broker/pkg/secretless/log"
 	"github.com/cyberark/secretless-broker/pkg/secretless/plugin/connector"
@@ -16,6 +20,36 @@ func (connector *SingleUseConnector) Connect(
 	credentialValuesByID connector.CredentialValuesByID,
 ) (net.Conn, error) {
 	// TODO: Magic here
-	return nil, nil
-}
 
+	// using DSN because it seems gomssql forces us to
+	c, err := mssql.NewConnector(
+		fmt.Sprintf(
+			"sqlserver://%s:%s@%s",
+			credentialValuesByID["user"],
+			credentialValuesByID["password"],
+			credentialValuesByID["address"],
+			),
+		)
+	if err != nil {
+		connector.logger.Errorf("bad connector: %s", err)
+		return nil, err
+	}
+
+	_conn, err := c.Connect(context.Background())
+	if err != nil {
+		connector.logger.Errorf("bad connect: %s", err)
+		return nil, err
+	}
+	conn := _conn.(*mssql.Conn)
+	backEndConn := conn.NetConn()
+
+	connector.logger.Infof("backend connection ready")
+
+	err = mssql.PerformFakeHandshakeForClient(clientConn)
+	if err != nil {
+		connector.logger.Errorf("bad PerformFakeHandshakeForClient: %s", err)
+		return nil, err
+	}
+
+	return backEndConn, nil
+}

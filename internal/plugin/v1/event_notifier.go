8dcef18a216ec63e053925715e36c7120537d466
Srdjan Grubor <sgnn7@sgnn7.org>
2019-10-22 14:09:54 -0500

    Removed out references to v1 Listener and Handler interfaces
    We no longer use these so they have been removed. Remnants of their use
    are still around in CRD parsing (code is autogenerated and checked in)
    and in parsing of configfiles (that code uses a separate struct layout
    for that).

--- internal/plugin/v1/event_notifier.go
+++ internal/plugin/v1/event_notifier.go
@@ -7,18 +7,8 @@ import (
 // EventNotifier is the interface which is used to pass event up from handlers/
 // listeners/managers back up to the main plugin manager
 type EventNotifier interface {
-	// NewConnection is called for each new client connection before being
-	// passed to a handler
-	NewConnection(Listener, net.Conn)
-
 	// ClientData is called for each inbound packet from clients
 	ClientData(net.Conn, []byte)
-
-	// CreateHandler is called after listener creates a new handler
-	CreateHandler(Handler, net.Conn)
-
-	// CreateListener is called for every listener created by Proxy
-	CreateListener(Listener)
 
 	// ResolveCredential is called when a provider resolves a variable
 	// TODO: unclear why we're reimplementing the StoredSecret functionality here...

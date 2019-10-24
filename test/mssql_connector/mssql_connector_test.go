d2769dd965851926521497c07f189f44b09b648e
orenbm <oren.benmeir@cyberark.com>
2019-10-24 14:28:39 -0400

    Test we cannot connect directly to MSSQL

--- test/mssql_connector/mssql_connector_test.go
+++ test/mssql_connector/mssql_connector_test.go
@@ -47,4 +47,25 @@ func TestMSSQLConnector(t *testing.T) {
 		assert.EqualValues(t, testInt, actualInt)
 		assert.EqualValues(t, testString, actualString)
 	})
+
+	t.Run("Cannot connect directly to MSSQL", func(t *testing.T) {
+		port := 1433
+		server := "mssql"
+
+		// Open the connection
+		connString := fmt.Sprintf("server=%s;port=%d;encrypt=disable", server, port)
+		conn, err := sql.Open("mssql", connString)
+		assert.Nil(t, err, "you can open the db connection")
+		if err != nil {
+			return
+		}
+		defer conn.Close()
+
+		// Prepare the test query - should fail because the connection is not authenticated
+		testInt := 1
+		testString := "abc"
+		_, err = conn.Prepare(fmt.Sprintf("select %d, '%s'", testInt, testString))
+		assert.NotNil(t, err, "Prepare fails without a successful login")
+		assert.Contains(t, err.Error(), "Login failed")
+	})
 }

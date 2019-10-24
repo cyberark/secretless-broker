402ddafa758f2400a63e1cda92154da83425f547
orenbm <oren.benmeir@cyberark.com>
2019-10-24 11:14:40 -0400

    Add test for the MSSQL Connector

---
+++ test/mssql_connector/mssql_connector_test.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"testing"
+
+	_ "github.com/denisenkom/go-mssqldb"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMSSQLConnector(t *testing.T) {
+	t.Run("Can connect to MSSQL through Secretless", func(t *testing.T) {
+		port := 2223
+		server := "secretless"
+
+		// Open the connection
+		connString := fmt.Sprintf("server=%s;port=%d;encrypt=disable", server, port)
+		conn, err := sql.Open("mssql", connString)
+		assert.Nil(t, err, "you can open the db connection")
+		if err != nil {
+			return
+		}
+		defer conn.Close()
+
+		// Prepare the test query
+		testInt := 1
+		testString := "abc"
+		stmt, err := conn.Prepare(fmt.Sprintf("select %d, '%s'", testInt, testString))
+		assert.Nil(t, err, "you can prepare the statement")
+		if err != nil {
+			return
+		}
+		defer stmt.Close()
+
+		// Execute the query
+		row := stmt.QueryRow()
+		var actualInt int64
+		var actualString string
+		err = row.Scan(&actualInt, &actualString)
+		assert.Nil(t, err, "you can read the queried values")
+		if err != nil {
+			return
+		}
+
+		// Test the returned values
+		assert.EqualValues(t, testInt, actualInt)
+		assert.EqualValues(t, testString, actualString)
+	})
+}

b8f500efe9c482f5cbbae876ad93b96e84470768
Srdjan Grubor <sgnn7@sgnn7.org>
2019-10-25 13:14:36 -0500

    Updated Juxtaposer configurations
    Newer configurations have different layouts and we needed a better set
    of defaults for Juxtaposer.

--- bin/juxtaposer/deploy/bootstrap.sh
+++ bin/juxtaposer/deploy/bootstrap.sh
@@ -1,15 +1,15 @@
 export CONFIG_TEMPLATE=pg
-export TEST_DURATION=96h
+export TEST_DURATION=1h
 
 export APP_NAME=juxtaposer-${CONFIG_TEMPLATE}
 
 export APP_SERVICE_ACCOUNT=secretless-xa
-export AUTHENTICATOR_ID=openshift/secretless-xa
+export AUTHENTICATOR_ID=openshift/xa-secretless
 export DAP_ACCOUNT=xa
 export DAP_NAMESPACE_NAME=xa-secretless
 export DAP_SSL_CERT_CONFIG_MAP=dap-ssl-cert
 export DOCKER_REGISTRY_PATH=REPLACEME
-export SECRETELESS_IMAGE="cyberark/secretless-broker:0.7.1-becf48d"
+export SECRETELESS_IMAGE="cyberark/secretless-broker:1.2.0"
 export TEST_APP_NAMESPACE_NAME=srdjan-secretless-xa
 
 OC_REPOSITORY="docker-registry.default.svc:5000/$TEST_APP_NAMESPACE_NAME"

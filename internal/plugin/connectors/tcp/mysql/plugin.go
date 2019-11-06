a9820bc9903c907811df19dce21df297da1eed7e
orenbm <oren.benmeir@cyberark.com>
2019-11-06 18:13:39 +0200

    Align PG and MySQL handlers with changes introduced in the MSSQL connector
    This commits changes some variable names & comments that were improved
    during the MSSQL connector implementation.

--- internal/plugin/connectors/tcp/mysql/plugin.go
+++ internal/plugin/connectors/tcp/mysql/plugin.go
@@ -7,26 +7,27 @@ import (
 	"github.com/cyberark/secretless-broker/pkg/secretless/plugin/connector/tcp"
 )
 
-// NewConnector returns a tcp.Connector which returns an authenticated connection
-// to a target service for each incoming client connection.
-// It is a required method on the tcp.Plugin interface.
-// The single argument passed in is of type connector.Resources. It contains
-// connector-specific config and a logger.
+/*
+NewConnector returns a tcp.Connector which returns an authenticated connection
+to a target service for each incoming client connection. It is a required
+method on the tcp.Plugin interface. The single argument passed in is of type
+connector.Resources. It contains connector-specific config and a logger.
+*/
 func NewConnector(conRes connector.Resources) tcp.Connector {
-	connectorFunc := func(
+	connect := func(
 		clientConn net.Conn,
 		credentialValuesByID connector.CredentialValuesByID,
 	) (backendConn net.Conn, err error) {
 		// singleUseConnector is responsible for generating the authenticated connection
 		// to the target service for each incoming client connection
 		singleUseConnector := &SingleUseConnector{
-			logger:   conRes.Logger(),
+			logger: conRes.Logger(),
 		}
 
 		return singleUseConnector.Connect(clientConn, credentialValuesByID)
 	}
 
-	return tcp.ConnectorFunc(connectorFunc)
+	return tcp.ConnectorFunc(connect)
 }
 
 // PluginInfo is required as part of the Secretless plugin spec. It provides

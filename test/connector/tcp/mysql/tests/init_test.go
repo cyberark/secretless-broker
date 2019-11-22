17a9d2a28ad62ecce0bbe431bc337c823c2474bb
Bradley Boutcher <btboutcher@icloud.com>
2019-11-22 12:00:03 -0500

    Reorganize integration tests and correct references
    As the name implies - we reorganize our various directories and
    update their references within their docker-compose files, etc.
    
    For a seperate commit, I think we can clean up our docker-compose files quite a bit.

---
+++ test/connector/tcp/mysql/tests/init_test.go
@@ -0,0 +1,25 @@
+package tests
+
+import (
+	"fmt"
+	"net"
+
+	"github.com/cyberark/secretless-broker/test/connector/tcp/mysql/pkg"
+	"github.com/cyberark/secretless-broker/test/util/testutil"
+)
+
+
+var RunTestCase testutil.RunTestCaseType
+// If the SecretlessHost is unavailable, bail out...
+//
+func init() {
+	_, err := net.LookupIP(testutil.SecretlessHost)
+	if err != nil {
+		fmt.Printf("ERROR: The secretless host '%s' wasn't found\n", testutil.SecretlessHost)
+		panic(err)
+	}
+
+	// generate TestSuiteLiveConfigurations
+	RunTestCase = testutil.NewRunTestCase(pkg.RunQuery)
+
+}
---
+++ test/connector/tcp/pg/tests/init_test.go
@@ -0,0 +1,24 @@
+package tests
+
+import (
+	"fmt"
+	"net"
+
+	"github.com/cyberark/secretless-broker/test/connector/tcp/pg/pkg"
+	"github.com/cyberark/secretless-broker/test/util/testutil"
+)
+
+
+var RunTestCase testutil.RunTestCaseType
+// If the SecretlessHost is unavailable, bail out...
+//
+func init() {
+	_, err := net.LookupIP(testutil.SecretlessHost)
+	if err != nil {
+		fmt.Printf("ERROR: The secretless host '%s' wasn't found\n", testutil.SecretlessHost)
+		panic(err)
+	}
+
+	RunTestCase = testutil.NewRunTestCase(pkg.RunQuery)
+
+}

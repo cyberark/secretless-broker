1572838291697ccee1fc9430f8968989bc2ff9f1
diverdane <dane.leblanc@cyberark.com>
2019-11-26 13:54:18 -0500

    Add UT test cases for generic connector validator
    This change adds UT test cases for the generic HTTP connector's
    validate method. This includes:
    - Creating a common getSampleConfig() testing function for parsing
      the UT sampleConfig
    - Add test cases for:
      * Valid username and password.
      * Missing required credential.
      * Credential that doesn't match corresponding validation string.
    
    Fixes: https://github.com/cyberark/secretless-broker/issues/1025

--- internal/plugin/connectors/http/generic/config_test.go
+++ internal/plugin/connectors/http/generic/config_test.go
@@ -1,15 +1,17 @@
 package generic
 
 import (
+	"strings"
 	"testing"
 
+	"github.com/pkg/errors"
 	"github.com/stretchr/testify/assert"
 )
 
-func sampleConfig() []byte {
+func sampleConfigYAML() []byte {
 	return []byte(`
-  credentialValidation:
-    username: '[^:]+'
+  credentialValidations:
+    username: '^[^:]+$'
   headers:
     Authorization: 'Basic {{ printf "%s:%s" .username .password | base64 }}'
     Name-With-Dashes: '{{ .username }}'
@@ -20,15 +22,21 @@ func sampleConfig() []byte {
 `)
 }
 
+func sampleConfig() (*config, error) {
+	cfgYAML, err := NewConfigYAML(sampleConfigYAML())
+	if err != nil {
+		return nil, errors.Wrap(err, "error parsing sample config YAML")
+	}
+	cfg, err := newConfig(cfgYAML)
+	if err != nil {
+		return nil, errors.Wrap(err, "error calling newConfig()")
+	}
+	return cfg, err
+}
+
 func Test_newConfig(t *testing.T) {
 	t.Run("creates expected headers", func(t *testing.T) {
-		cfgYAML, err := NewConfigYAML(sampleConfig())
-		if err != nil {
-			assert.Fail(t, "sampleConfig should never fail")
-			return
-		}
-		cfg, err := newConfig(cfgYAML)
-
+		cfg, err := sampleConfig()
 		assert.NoError(t, err)
 		if err != nil {
 			return
@@ -52,4 +60,55 @@ func Test_newConfig(t *testing.T) {
 		assert.Equal(t, "Basic Sm9uYWg6c2VjcmV0", headers["Authorization"])
 	})
 }
-// TODO: Add validation tests
+
+func Test_validate(t *testing.T) {
+	testCases := []struct {
+		description string
+		creds       map[string][]byte
+		expErrStr   string
+	}{
+		{
+			description: "validates good credentials",
+			creds: map[string][]byte{
+				"username": []byte("Jonah"),
+				"password": []byte("secret"),
+			},
+			expErrStr: "",
+		},
+		{
+			description: "errors on missing required credential",
+			creds: map[string][]byte{
+				"password": []byte("secret"),
+			},
+			expErrStr: "missing required credential",
+		},
+		{
+			description: "errors on invalid credential",
+			creds: map[string][]byte{
+				"username": []byte("Jon:ah"),
+				"password": []byte("secret"),
+			},
+			expErrStr: "doesn't match pattern",
+		},
+	}
+
+	cfg, err := sampleConfig()
+	assert.NoError(t, err)
+	if err != nil {
+		return
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.description, func(t *testing.T) {
+			err = cfg.validate(tc.creds)
+			if tc.expErrStr == "" {
+				assert.NoError(t, err)
+				return
+			}
+			assert.Error(t, err)
+			if err != nil {
+				assert.True(t, strings.Contains(err.Error(), tc.expErrStr))
+			}
+		})
+	}
+}

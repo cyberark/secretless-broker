09bc14a79e84d9c1ea74c4dabc8f773bb50ddda1
Bradley Boutcher <btboutcher@icloud.com>
2019-12-05 12:22:53 -0500

    Reformat MsSQL Connector
    Since we use the third party connector for much of this process, it makes sense to reduce the code base for our overhead as much as possible and further reduce the complexity of it all.
    
    - Removed the authentication.go file, since we are not performing any real authentication there
    - Renamed authentication.go to authentication_responses.go, since it currently contains pre made responses. This can be removed once we dynamically return responses from mssql (#1013, #1014, and #1016)
    - Combined Connect and ConnectToBackend since their functionality, purpose, and signatures are the same. This can probably be further reduced or have better naming conventions.
    - Added comments detailing the steps we take within the Connect phases performed by Secretless
    - Moved performLoginHandshake() from authentication.go to connector.go, and renamed to customizePreloginRequest, since that is all it's actually doing and still has a purpose in that regard
    - Added some comments specifying story work where applicable

--- internal/plugin/connectors/tcp/mssql/connector.go
+++ internal/plugin/connectors/tcp/mssql/connector.go
@@ -33,28 +33,47 @@ func (connector *SingleUseConnector) Connect(
 
 	connector.clientConn = clientConn
 
-	err := connector.performPreLoginHandshake()
+	// 1. Customize PreLogin Handshake to not include ecnryption
+	err := connector.customizePreLoginRequest()
 	if err != nil {
 		connector.logger.Errorf("Failed to handle client prelogin request: %s", err)
 		connector.sendErrorToClient()
 		return nil, err
 	}
 
+	// 2. Prepare connection details formatted for MsSQL
 	connDetails, err := NewConnectionDetails(credentialValuesByID)
 	if err != nil {
 		connector.sendErrorToClient()
 		return nil, err
 	}
 
-	err = connector.ConnectToBackend(dataSourceName(connDetails))
+	// 3. Create a new MsSQL connector
+	// Using DSN (Data Source Name) string because gomssql forces us to.
+	//
+	// NOTE: Secretless has some unfortunate naming collisions with the
+	// go-mssqldb driver package.  The driver package has its own concept of a
+	// "connector", and its connectors also have a "Connect" method.
+	driverConnector, err := mssql.NewConnector(dataSourceName(connDetails))
 	if err != nil {
-		connector.logger.Errorf("Failed to connect to backend: %s", err)
+		connector.logger.Errorf("Failed to create a go-mssqldb connector: %s", err)
 		connector.sendErrorToClient()
 		return nil, err
 	}
 
-	// Now that the backend connection is authenticated, send the client
-	// a successful authentication response.
+	// 4. Kick off authentication through our third party connector
+	driverConn, err := driverConnector.Connect(context.Background())
+	if err != nil {
+		connector.logger.Errorf("failed to connect to mssql server: %s", err)
+		connector.sendErrorToClient()
+		return nil, err
+	}
+
+	// TODO: 	5.	Send the prelogin response to the client (#1014)
+	// TODO: 	6.	Send the login response to the client 	 (#1016)
+	// TODO: 	Alt. Send an error to the client and return nil. (#1013)
+
+	// TODO: Replace this with an actual 'ok' message from the server
 	if _, err = clientConn.Write(connector.CreateAuthenticationOKMessage()); err != nil {
 		connector.logger.Errorf("Failed to send a successful authentication"+
 			" response to the client"+
@@ -63,39 +82,52 @@ func (connector *SingleUseConnector) Connect(
 		return nil, err
 	}
 
-	return connector.backendConn, nil
-}
-
-// ConnectToBackend establishes the connection to the target database and sets
-// the backendConnection field.
-func (connector *SingleUseConnector) ConnectToBackend(dataSourceName string) error {
-	var err error
-
-	// Using DSN (Data Source Name) string because gomssql forces us to.
-	//
-	// NOTE: Secretless has some unfortunate naming collisions with the
-	// go-mssqldb driver package.  The driver package has its own concept of a
-	// "connector", and its connectors also have a "Connect" method.
-	driverConnector, err := mssql.NewConnector(dataSourceName)
-	if err != nil {
-		connector.logger.Errorf("Failed to create a go-mssqldb connector: %s", err)
-		return err
-	}
-
-	driverConn, err := driverConnector.Connect(context.Background())
-	if err != nil {
-		connector.logger.Errorf("failed to connect to mssql server: %s", err)
-		return err
-	}
-
 	// Verify the driverConn is an mssql driverConn object and get its underlying transport
 	mssqlConn := driverConn.(*mssql.Conn)
 	connector.backendConn = mssqlConn.NetConn()
 
+	return connector.backendConn, nil
+}
+
+func (connector *SingleUseConnector) customizePreLoginRequest() error {
+	// using the default packet size of 4096
+	// (see https://docs.microsoft.com/en-us/sql/database-engine/configure-windows/configure-the-network-packet-size-server-configuration-option)
+	clientBuffer := mssql.NewTdsBuffer(4096, connector.clientConn)
+	preloginRequest, err := mssql.ReadPreloginWithPacketType(clientBuffer, mssql.PackPrelogin)
+	if err != nil {
+		return fmt.Errorf("failed to read prelogin request: %s", err)
+	}
+
+	// According to the mssql docs, The client can use the VERSION returned from
+	// the server to determine which features SHOULD be enabled or disabled.
+	// TODO: Extract version from server instead of hard-coded one.
+	// we use now the version of the sql server in the test
+	preloginRequest[mssql.PreloginVERSION] = []byte{0x0e, 0x00, 0x0c, 0xa6, 0x00, 0x00}
+
+	// Remove Client SSL Capability from Server Handshake Packet
+	// to force client to connect to Secretless without SSL
+	preloginRequest[mssql.PreloginENCRYPTION] = []byte{mssql.EncryptNotSup}
+
+	// According to the docs, this value should be empty when being sent from
+	// the server to the client.
+	preloginRequest[mssql.PreloginTHREADID] = []byte{}
+
+	err = mssql.WritePreloginWithPacketType(clientBuffer, preloginRequest, mssql.PackReply)
+	if err != nil {
+		return fmt.Errorf("failed to write prelogin response: %s", err)
+	}
+
+	// we actually don't need the client's handshake response.
+	// we just need for them to not be blocked
+	err = clientBuffer.ReadNextPacket()
+	if err != nil {
+		return fmt.Errorf("failed to read client login message: %s", err)
+	}
+
 	return nil
 }
 
-// TODO: add ability to receive an MSSQL error and send it to the client
+// TODO: Add ability to receive an MSSQL error and send it to the client (#1013)
 func (connector *SingleUseConnector) sendErrorToClient() {
 	mssqlError := connector.CreateGenericErrorMessage()
 	if _, e := connector.clientConn.Write(mssqlError); e != nil {

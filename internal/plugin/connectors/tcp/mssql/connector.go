b82cee28e1bb7c9e150d3cbdf01490058c1de81b
Bradley Boutcher <btboutcher@icloud.com>
2019-12-09 14:12:16 -0500

    Add appropriate comments
    - Diagram depicting current state of the connector
    - Class comment
    - Individual changes to certain comments throughout the process
    - Rename several variables to better match their function

--- internal/plugin/connectors/tcp/mssql/connector.go
+++ internal/plugin/connectors/tcp/mssql/connector.go
@@ -2,9 +2,9 @@ package mssql
 
 import (
 	"context"
+	"database/sql/driver"
 	"fmt"
 	"net"
-	"database/sql/driver"
 
 	"github.com/cyberark/secretless-broker/pkg/secretless/log"
 	"github.com/cyberark/secretless-broker/pkg/secretless/plugin/connector"
@@ -13,6 +13,84 @@ import (
 	mssql "github.com/denisenkom/go-mssqldb"
 )
 
+/*
+
+This connector acts as a layer between the client and the driver, where the
+driver handles communication with the server itself. As such, some stages are
+handled independently of secretless, while other stages require the interception
+or modification of the respective requests or responses.
+
+Overview of the connection process
+
++---------+              +-------------+                      +---------+               +-------+
+| Client  |              | Secretless  |                      | Driver  |               | MsSQL |
++---------+              +-------------+                      +---------+               +-------+
+     |                          |                                  |                        |
+     | Prelogin Request         |                                  |                        |
+     |------------------------->|                                  |                        |
+     |                          |                                  |                        |
+     |                          | Connect(Context)                 |                        |
+     |                          |--------------------------------->|                        |
+     |                          | ----------------------------\    |                        |
+     |                          |-| Context contains channels |    |                        |
+     |                          | | for intercepting data     |    |                        |
+     |                          | |---------------------------|    |                        |
+     |                          |                                  | Prelogin Request       |
+     |                          |                                  |----------------------->|
+     |                          |                                  |                        |
+     |                          |                                  |      Prelogin Response |
+     |                          |                                  |<-----------------------|
+     |                          |                                  |                        |
+     |                          |      Intercept Prelogin Response |                        |
+     |                          |<---------------------------------|                        |
+     |                          |                                  |                        |
+     |                          | Modify Prelogin Response         |                        |
+     |                          |------------------------          |                        |
+     |                          |                       |          |                        |
+     |                          |<-----------------------          |                        |
+     |                          |                                  |                        |
+     |        Prelogin Response |                                  |                        |
+     |<-------------------------|                                  |                        |
+     |                          |                                  |                        |
+     |                          |                                  | Handshake Request      |
+     |                          |                                  |----------------------->|
+     |                          |                                  |                        |
+     |                          |                                  |     Handshake Response |
+     |                          |                                  |<-----------------------|
+     |                          |                                  |                        |
+     |                          |                                  | Login Request          |
+     |                          |                                  |----------------------->|
+     |                          |                                  |                        |
+     |                          |                                  |         Login Response |
+     |                          |                                  |<-----------------------|
+     |                          |                                  |                        |
+     |                          |         Login success or failure |                        |
+     |                          |<---------------------------------|                        |
+     |                          |                                  |                        |
+     |    Login Response (Fake) |                                  |                        |
+     |<-------------------------|                                  |                        |
+     |                          |                                  |                        |
+
+	Note: The above diagram was created using https://textart.io/sequence and the
+	following source:
+
+	object Client Secretless Driver MsSQL
+	Client->Secretless: Prelogin Request
+	Secretless->Driver: Connect(context)
+	note right of Secretless: Context contains channels \n for intercepting data
+	Driver->MsSQL: Prelogin Request
+	MsSQL->Driver: Prelogin Response
+	MsSQL->Secretless: Intercept Prelogin Response
+	Secretless->Secretless: Modify Prelogin Response
+	Secretless->Client: Prelogin Response
+	Driver->MsSQL:Handshake Request
+	MsSQL->Driver: Handshake Response
+	Driver->MsSQL: Login Request
+	MsSQL->Driver: Login Response
+	Driver->Secretless: Login success or failure
+	Secretless->Client: Login Response (Premade)
+ */
+
 // SingleUseConnector is used to create an authenticated connection to an MSSQL target
 type SingleUseConnector struct {
 	backendConn net.Conn
@@ -20,14 +98,20 @@ type SingleUseConnector struct {
 	logger      log.Logger
 }
 
-const bufferSize uint16 = 4086
+// https://docs.microsoft.com/en-us/sql/database-engine/configure-windows/configure-the-network-packet-size-server-configuration-option
+// Default packet size remains at 4096 bytes
+const bufferSize uint16 = 4096
 
 // Connect implements the tcp.Connector func signature
 //
 // It is the main method of the SingleUseConnector. It:
 //   1. Constructs connection details from the provided credentials map.
-//   2. Dials the backend using credentials.
-//   3. Runs through the connection phase steps to authenticate.
+//   2. Reads the client PreLogin request
+//   3. Adds channels to the context that can intercept data from the driver
+//   4. Initiates authentication and connection to MsSQL through the third-party driver
+//   5. Intercepts PreLogin response from the driver
+//	 6. Customizes the PreLogin response to meet Secretless standards
+//		and sends it to the user
 //
 // Connect requires "host", "port", "username" and "password" credentials.
 func (connector *SingleUseConnector) Connect(
@@ -37,92 +121,96 @@ func (connector *SingleUseConnector) Connect(
 
 	connector.clientConn = clientConn
 
-	// Read the prelogin request from the client, but we don't need to do anything
-	// we just need to make sure the client isn't blocked
+	// Secretless _is_ the client with respect to the server, and there is nothing in the
+	// pre-login handshake that needs to be passed along.  Secretless simply reads
+	// it from the client and throws it away, so that client can advance to the next
+	// stage of the process.  Otherwise the client would block forever waiting for its
+	// pre-login handshake to be read.
 	clientBuffer := mssql.NewTdsBuffer(bufferSize, connector.clientConn)
 	_, err := mssql.ReadPreloginWithPacketType(clientBuffer, mssql.PackPrelogin)
 	if err != nil {
-		return nil, fmt.Errorf("failed to read prelogin request: %s", err)
+		return connector.sendError("failed to read prelogin request: %s", err)
 	}
 
-	// 2. Prepare connection details from the client, formatted for MsSQL
+	// Prepare connection details from the client, formatted for MsSQL
 	connDetails, err := NewConnectionDetails(credentialValuesByID)
 	if err != nil {
-		connector.sendErrorToClient()
-		return nil, err
+		return connector.sendError("Unable to create new connection details: %s", err)
 	}
 
-	// 3. Create a new MsSQL connector
-	//    Using DSN (Data Source Name) string because gomssql forces us to.
+	// Create a new MsSQL connector
+	// Using DSN (Data Source Name) string because gomssql forces us to.
 	//
 	// NOTE: Secretless has some unfortunate naming collisions with the
 	// go-mssqldb driver package.  The driver package has its own concept of a
 	// "connector", and its connectors also have a "Connect" method.
 	driverConnector, err := mssql.NewConnector(dataSourceName(connDetails))
 	if err != nil {
-		connector.logger.Errorf("Failed to create a go-mssqldb connector: %s", err)
-		connector.sendErrorToClient()
-		return nil, err
+		return connector.sendError("Failed to create a go-mssqldb connector: %s", err)
 	}
 
-	// Create the context with a channel we will use
-	parentContext := context.Background()
-	ch := make(chan map[uint8][]byte)
+	// Create the context for our connection
+	ctx := context.Background()
 
-	// Set a 'marker' for when the driver has finished connecting
-	driverConnSet := make(chan struct {})
+	// Create a channel for receiving the prelogin response through the context
+	preLoginResponseChannel := make(chan map[uint8][]byte)
+
+	// Set a 'marker' for when the driver has finished connecting to the server
+	connectPhaseFinished := make(chan struct {})
 
 	// Add channels to the context, to retrive information from the driver
-	childContext := context.WithValue(parentContext, ctxtypes.PreLoginResponseKey, ch)
+	loginContext := context.WithValue(ctx, ctxtypes.PreLoginResponseKey,
+		preLoginResponseChannel)
 
 	// Build a new driver connection
 	var driverConn driver.Conn
 
 	go func() {
 		// Kick off authentication through our third party connector
-		driverConn, err = driverConnector.Connect(childContext)
-		driverConnSet <- struct{}{}
+		driverConn, err = driverConnector.Connect(loginContext)
+		connectPhaseFinished <- struct{}{}
 
 		if err != nil {
-			connector.logger.Errorf("failed to connect to mssql server: %s", err)
-			connector.sendErrorToClient()
+			_, err = connector.sendError("failed to connect to mssql server: %s", err)
 		}
 	} ()
 
 	// Blocks continuation until we've received the preLoginResponse from the driver
-	preloginResponse := <- ch
+	preloginResponse := <- preLoginResponseChannel
+
+	// Since the communication between the client and Secretless must be unencrypted,
+	// we fool the client into thinking that it's talking to a server that does not support
+	// encryption.
 	preloginResponse[mssql.PreloginENCRYPTION] = []byte{mssql.EncryptNotSup}
 
 	// Write the prelogin packet back to the user
 	err = mssql.WritePreloginWithPacketType(clientBuffer, preloginResponse, mssql.PackReply)
 	if err != nil {
-		return nil, fmt.Errorf("failed to write prelogin response: %s", err)
+		return connector.sendError("failed to write prelogin response: %s", err)
 	}
 
-	// We actually don't need the client's handshake response;
-	// we just need for them to not be blocked
+	// Just like above, we don't need to concern ourselves with what the client would
+	// typically be sending to the server, since we are handling all communication with
+	// the server ourselves. We read the next packet so the client isn't blocked,
+	// while the driver handles the handshake.
 	err = clientBuffer.ReadNextPacket()
 	if err != nil {
-		connector.logger.Errorf("Failed to handle client prelogin request: %s", err)
-		connector.sendErrorToClient()
-		return nil, err
+		return connector.sendError("Failed to handle client prelogin request: %s", err)
 	}
 
-	// TODO: 	5.	Send the prelogin response to the client (#1014)
-	// TODO: 	6.	Send the login response to the client 	 (#1016)
-	// TODO: 	Alt. Send an error to the client and return nil. (#1013)
+	// TODO: 	Send the login response to the client 	 (#1016)
+	// TODO: 	Verify appropriate errors are passed to the client (#1013)
 
 	// TODO: 	Replace this with an actual 'ok' message from the server
+	//			once login completes within the driver
 	if _, err = clientConn.Write(connector.CreateAuthenticationOKMessage()); err != nil {
-		connector.logger.Errorf("Failed to send a successful authentication"+
+		return connector.sendError("Failed to send a successful authentication"+
 			" response to the client"+
 			": %s", err)
-		connector.sendErrorToClient()
-		return nil, err
 	}
 
 	// Block continuation until driver has completed connection
-	<- driverConnSet
+	<- connectPhaseFinished
 
 	// Verify the driverConn is an mssql driverConn object and get its underlying transport
 	mssqlConn := driverConn.(*mssql.Conn)
@@ -132,6 +220,13 @@ func (connector *SingleUseConnector) Connect(
 }
 
 
+// TODO: Add ability to receive an MSSQL error and send it to the client (#1013)
+func (connector *SingleUseConnector) sendError(message string, err error) (net.Conn, 
+	error){
+	connector.logger.Errorf(message, err)
+	connector.sendErrorToClient()
+	return nil, err
+}
 
 // TODO: Add ability to receive an MSSQL error and send it to the client (#1013)
 func (connector *SingleUseConnector) sendErrorToClient() {

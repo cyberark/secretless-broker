2198ad40c8294babf4db261d91be5115e7ebf451
Geri Jennings <geri.jennings@cyberark.com>
2020-01-08 14:37:32 -0500

    Bump version to 1.4.2

--- pkg/secretless/version.go
+++ pkg/secretless/version.go
@@ -4,7 +4,7 @@ import "fmt"
 
 // Version field is a SemVer that should indicate the baked-in version
 // of the broker
-var Version = "1.4.1"
+var Version = "1.4.2"
 
 // Tag field denotes the specific build type for the broker. It may
 // be replaced by compile-time variables if needed to provide the git

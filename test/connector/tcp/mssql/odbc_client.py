099e93adb686516caa88054bf972cb49d37e747d
Kumbirai Tanekha <kumbirai.tanekha@gmail.com>
2020-01-23 18:05:32 +0000

    mssql: add client params propagation integration tests

--- test/connector/tcp/mssql/odbc_client.py
+++ test/connector/tcp/mssql/odbc_client.py
@@ -9,6 +9,7 @@ parser.add_argument('--server', default="127.0.0.1",
 parser.add_argument('--database', default="")
 parser.add_argument('--username', default="")
 parser.add_argument('--password', default="")
+parser.add_argument('--application-intent', default="readwrite")
 parser.add_argument('--query', default="",
                     help='query to execute (default: "")')
 
@@ -19,8 +20,9 @@ CONN_INFO = {
     "database": args.database,
     "username": args.username,
     "password": args.password,
+    "application_intent": args.application_intent,
 }
-CONN_TEMPLATE_STR="DRIVER={{ODBC Driver 17 for SQL Server}};SERVER={server};DATABASE={database};UID={username};PWD={password};"
+CONN_TEMPLATE_STR="DRIVER={{ODBC Driver 17 for SQL Server}};SERVER={server};DATABASE={database};UID={username};PWD={password};applicationintent={application_intent}"
 conn_string = CONN_TEMPLATE_STR.format(**CONN_INFO)
 SQL_ATTR_CONNECTION_TIMEOUT = 113
 login_timeout = 2

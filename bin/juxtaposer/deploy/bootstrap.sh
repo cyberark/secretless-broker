9e029533cd24e48b4c9bf7253ff148c5cbb32e11
diverdane <dane.leblanc@cyberark.com>
2020-02-03 20:05:09 -0500

    Modify juxtaposer to run with secretless plus PG
    Sensitive stuff is redacted with the pattern '*****REDACTED*****'.

--- bin/juxtaposer/deploy/bootstrap.sh
+++ bin/juxtaposer/deploy/bootstrap.sh
@@ -1,16 +1,16 @@
 export CONFIG_TEMPLATE=pg
-export TEST_DURATION=1h
+export TEST_DURATION=96h
 
-export APP_NAME=juxtaposer-${CONFIG_TEMPLATE}
+export APP_NAME=juxtaposer-pg
 
 export APP_SERVICE_ACCOUNT=secretless-xa
 export AUTHENTICATOR_ID=openshift/xa-secretless
 export DAP_ACCOUNT=xa
 export DAP_NAMESPACE_NAME=xa-secretless
 export DAP_SSL_CERT_CONFIG_MAP=dap-ssl-cert
-export DOCKER_REGISTRY_PATH=REPLACEME
-export SECRETELESS_IMAGE="cyberark/secretless-broker:1.2.0"
-export TEST_APP_NAMESPACE_NAME=srdjan-secretless-xa
+export DOCKER_REGISTRY_PATH=*****REDACTED*****
+export SECRETELESS_IMAGE="cyberark/secretless-broker:1.4.2"
+export TEST_APP_NAMESPACE_NAME=dane-secretless-xa
 
 OC_REPOSITORY="docker-registry.default.svc:5000/$TEST_APP_NAMESPACE_NAME"
 TAG_NAME="$TEST_APP_NAMESPACE_NAME"

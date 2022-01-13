a173adbdc93f8328419d8ce8979c3357db6edbe0
Kumbirai Tanekha <kumbirai.tanekha@gmail.com>
2022-01-13 14:39:04 +0000

    Clean up client options pg tests

--- test/connector/tcp/pg/tests/client_options_test.go
+++ test/connector/tcp/pg/tests/client_options_test.go
@@ -20,17 +20,18 @@ func TestClientOptions(t *testing.T) {
 	l := testutil.FindLiveConfiguration(ac)
 
 	options := []string{"dbname=postgres"}
+	runQuery := genQueryRunner(l.Host(), l.ToPortString())
 
 	t.Run("dbname", func(t *testing.T) {
 		t.Run("exisiting", func(t *testing.T) {
-			output, err := runQuery(l.Host(), l.ToPortString(), nil, options, "SELECT current_database();")
+			output, err := runQuery(nil, options, "SELECT current_database();")
 
 			assert.NoError(t, err)
 			assert.Contains(t, output, "postgres")
 		})
 
 		t.Run("not found", func(t *testing.T) {
-			output, err := runQuery(l.Host(), l.ToPortString(), nil, []string{"dbname=notfound"}, "SELECT current_database();")
+			output, err := runQuery(nil, []string{"dbname=notfound"}, "SELECT current_database();")
 
 			assert.Error(t, err)
 			assert.Contains(t, output, `database "notfound" does not exist`)
@@ -48,21 +49,21 @@ func TestClientOptions(t *testing.T) {
 			encoding := "latin1"
 			query := fmt.Sprintf("select value from test.encodings where encoding='%s'", encoding)
 
-			output, err := runQuery(l.Host(), l.ToPortString(), genEnvs(encoding), options, query)
+			output, err := runQuery(genEnvs(encoding), options, query)
 
 			assert.NoError(t, err)
 			assert.Contains(t, output, "tést")
 		})
 
 		t.Run("defaults to utf8 when not set", func(t *testing.T) {
-			output, err := runQuery(l.Host(), l.ToPortString(), nil, options, "SHOW client_encoding;")
+			output, err := runQuery(nil, options, "SHOW client_encoding;")
 
 			assert.NoError(t, err)
 			assert.Contains(t, output, "UTF8")
 		})
 
 		t.Run("propagates to target server", func(t *testing.T) {
-			output, err := runQuery(l.Host(), l.ToPortString(), genEnvs("euc-jp"), options, "SHOW client_encoding;")
+			output, err := runQuery(genEnvs("euc-jp"), options, "SHOW client_encoding;")
 
 			assert.NoError(t, err)
 			assert.Contains(t, output, "EUC_JP")
@@ -70,17 +71,19 @@ func TestClientOptions(t *testing.T) {
 	})
 }
 
-func runQuery(host string, port string, envs []string, options []string, query string) (string, error) {
-	args := []string{
-		fmt.Sprintf("--host=%s", host),
-		fmt.Sprintf("--port=%s", port),
-		"-c", query,
-		strings.Join(append([]string{"sslmode=disable"}, options...), " "),
+func genQueryRunner(host string, port string) func(envs []string, options []string, query string) (string, error) {
+	return func(envs, options []string, query string) (string, error) {
+		args := []string{
+			fmt.Sprintf("--host=%s", host),
+			fmt.Sprintf("--port=%s", port),
+			"-c", query,
+			strings.Join(append([]string{"sslmode=disable"}, options...), " "),
+		}
+
+		c := exec.Command("psql", args...)
+		c.Env = append(c.Env, envs...)
+		cmdOut, err := c.CombinedOutput()
+
+		return string(cmdOut), err
 	}
-
-	c := exec.Command("psql", args...)
-	c.Env = append(c.Env, envs...)
-	cmdOut, err := c.CombinedOutput()
-
-	return string(cmdOut), err
 }
